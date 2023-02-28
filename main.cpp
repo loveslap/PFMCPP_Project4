b04049d80feb50046bbaa56c914ef5b613784ec7
Charles Spencer <83370974+loveslap@users.noreply.github.com>
2023-02-28 11:12:17 +0000

    passed diffchecker

--- main.cpp
+++ main.cpp
@@ -116,7 +116,7 @@ float FloatType::multiply( float lhs, float rhs )
 float FloatType::divide( float lhs, float rhs )
 {
     if( rhs == 0.0f )
-        std::cout <<   "\nwarning, floating point division by zero returns 'inf'";
+        std::cout <<   "\nwarning, floating point division by zero returns 'inf' !\n";
     return lhs / rhs;   
 }
 
@@ -135,7 +135,7 @@ double DoubleType::multiply( double lhs, double rhs )
 double DoubleType::divide( double lhs, double rhs )
 {
     if( rhs == 0.0 ) 
-        std::cout <<   "\nwarning, floating point division by zero returns 'inf'";
+        std::cout <<   "\nwarning, floating point division by zero returns 'inf' !\n";
     return lhs / rhs;   
 }
 
